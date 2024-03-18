7147dca8ff96a447858e8a061a7bafdb37c73891
RichardR <richardramos112094@gmail.com>
2024-03-19 05:07:23 +0800

    update data csv to locale

--- app/components/FetchCsv.jsx
+++ app/components/FetchCsv.jsx
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 
 export default async function fetchCsv() {
   // fetch csv data
-  const data = await d3.csv('./data.csv')
+  const data = await d3.csv('https://data-visual-v2.vercel.app/data.csv')
     
   return data
 }
