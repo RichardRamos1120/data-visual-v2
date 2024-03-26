018519f5759f1de0eb484e8e1270751164393109
Richard Ramos <38115103+RichardRamos1120@users.noreply.github.com>
2024-03-27 05:41:25 +0800

    Update page.jsx

--- app/d/page.jsx
+++ app/d/page.jsx
@@ -1,6 +1,7 @@
-
-
-
+import Topdata from "../components/Topdata";
+import fetchCsv from "../components/FetchCsv";
+import BarChart from "../components/BarChart";
+import Link from "next/link";
 
 async function getData () {
 
