295faf1eb6aad67e2de1947153709c5d791c1263
RichardR <richardramos112094@gmail.com>
2024-03-27 20:38:49 +0800

    add zones

--- app/components/Topdata.jsx
+++ app/components/Topdata.jsx
@@ -20,7 +20,7 @@ export default function Topdata() {
         <div className="card-container__main">
 
             <div className="card-container-1">
-            <h2 className="card-container__main__h2">2</h2>
+            <h2 className="card-container__main__h2">02</h2>
             <p className="container__main__p">Total Languages</p>
             </div>
             <img src="/fluent_data-area-20-filled.svg" alt="card-image" className="card-img" />
@@ -29,8 +29,8 @@ export default function Topdata() {
         <div className="card-container__main">
     
     <div className="card-container-1">
-    <h2 className="card-container__main__h2">500</h2>
-    <p className="card-container__main__p">Total Respondents</p>
+    <h2 className="card-container__main__h2">05</h2>
+    <p className="card-container__main__p">Total Zones</p>
     </div>
     <img src="/fluent_data-area-20-filled.svg" alt="card-image" className="card-img" />
 </div>
